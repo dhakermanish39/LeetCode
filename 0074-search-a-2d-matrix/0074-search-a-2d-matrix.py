136f3e8706a081dc70957d1fea55130616af9c50
dhakermanish39 <dhakermanish39@gmail.com>
2026-07-22 19:00:42 +0530

    Time: 0 ms (100%), Space: 12.7 MB (2.02%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m=len(matrix)
+        n=len(matrix[0])
+        for i in range(m):
+            if matrix[i][n-1]<target:
+                pass
+            else:
+                for j in range(n):
+                    if matrix[i][j]==target:
+                        return True
+        return False                    

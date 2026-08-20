65fb241ae50b8f361009c59ab2b87e9706ad05cb
dhakermanish39 <dhakermanish39@gmail.com>
2026-08-20 23:24:45 +0530

    Time: 0 ms (100%), Space: 12.5 MB (21.04%) - LeetHub

---
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        c=0
+        for i in nums:
+            if i%3!=0:
+                c+=1
+        return c        
+
+        

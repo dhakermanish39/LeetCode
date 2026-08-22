8c21230699b9aa5345dacc498d410918343fbe1d
dhakermanish39 <dhakermanish39@gmail.com>
2026-08-23 02:12:04 +0530

    Time: 746 ms (8.04%), Space: 13.7 MB (31.33%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,18 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        count=0
         result=0
         s=[]
         for i in st:
             if i not in s:
                 s.append(i)
-                
-                result=max(result,len(s))
+                count+=1
+                result=max(result,count)
             else:
                 
-                #result=max(result,len(s))
+                result=max(result,len(s))
                 while i in s:
                     s.pop(0)
-                s.append(i)    
+                    count-=1
+                s.append(i)  
+                count+=1  
                     
         return result

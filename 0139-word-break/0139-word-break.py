7e6019c66acfc4d419f46bffa8a58996e2c5970c
dhakermanish39 <dhakermanish39@gmail.com>
2026-08-22 14:54:33 +0530

    Time: 3 ms (89.36%), Space: 12.3 MB (73.52%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -13,15 +13,17 @@ class Solution(object):
             r+=len(i)    
         return  True  if len(s)>=r else False
         """
-        dp = [False for i in range(len(s)+1)]
-        dp[0] = True
-
-        wordDict = set(wordDict)
-
-        for i in range(1, len(s) + 1):
-            for j in range(i):
-                if dp[j] and s[j:i] in wordDict:
-                    dp[i] = True
-                    break
-
-        return dp[len(s)]
+        result = [False for i in range(len(s))]
+        wd = set(wordDict)
+        for i in range (len(s)):
+            if s[:i+1] in wd:
+                result[i] = True
+                continue
+            for j in range(i-1, -1, -1):
+                if result[j]:
+                
+                    if s[j+1:i+1] in wd:
+                        result[i] = True
+                        break
+     
+        return result[-1]

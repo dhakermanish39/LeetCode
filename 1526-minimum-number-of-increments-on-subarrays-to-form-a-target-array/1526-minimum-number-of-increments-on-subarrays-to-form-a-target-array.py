0173843c197345ce5137e1b0bd13d0f8393f7402
dhakermanish39 <dhakermanish39@gmail.com>
2026-08-24 22:04:14 +0530

    Time: 48 ms (52.94%), Space: 19.6 MB (25.49%) - LeetHub

---
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minNumberOperations(self, target):
+        """
+        :type target: List[int]
+        :rtype: int
+        """
+        ans=target[0]
+        for i in range(1,len(target)):
+            if target[i]>target[i-1]:
+                ans=ans+ target[i]-target[i-1]
+        return ans        
+        

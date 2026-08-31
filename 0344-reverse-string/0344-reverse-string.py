84ac919d9ea4d5da9123d492be3be188230945f0
dhakermanish39 <dhakermanish39@gmail.com>
2026-08-31 19:36:20 +0530

    Time: 2 ms (61.19%), Space: 19.7 MB (76.5%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        a=0
+        b=len(s)-1
+        while a<b:
+            s[a],s[b]=s[b],s[a]
+            a+=1
+            b-=1
+            
+        

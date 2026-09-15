c7309296d936949f26959aa668d14985a21a06b1
dhakermanish39 <dhakermanish39@gmail.com>
2026-09-15 16:09:48 +0530

    Time: 534 ms (33.04%), Space: 69.3 MB (32.74%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numDistinct(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        
+        ans=[]
+        for i in s:
+            if i in t:
+                ans.append(i)
+        return len(ans)-len(set(t))+1     
+        """
+class Solution(object):
+    def numDistinct(self, s, t):
+        m = len(s)
+        n = len(t)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        return dp[m][n]           
+        

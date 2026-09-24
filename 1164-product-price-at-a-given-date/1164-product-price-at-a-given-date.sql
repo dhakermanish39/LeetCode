62994732f291af3169ab8058b989d5fef3098ce2
dhakermanish39 <dhakermanish39@gmail.com>
2026-09-24 09:50:14 +0530

    Time: 1567 ms (7.86%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,15 +1,12 @@
-with cte as (
+with temp as (
     select product_id
         , max(change_date) as max_change_date
     from products 
     where change_date <= '2019-08-16'
     group by product_id
 )
-select distinct a.product_id
-    , case when max_change_date is null then 10 else new_price end as price 
-from products a
-left join cte b
-on a.product_id = b.product_id
-where change_date = max_change_date
-or b.product_id is null
+select distinct Products.product_id
+    , case when max_change_date is null then 10 else (select new_price  from Products where product_id=temp.product_id and change_date=temp.max_change_date)  end as price 
+from temp right join  Products on 
+temp.product_id=Products.product_id
 ;
